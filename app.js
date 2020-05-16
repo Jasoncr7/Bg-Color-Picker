59b11ca05943b503ee037ab2b419228b8d622446
Your Name <you@example.com>
2020-05-16 16:47:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const blue = document.querySelector('.blue');
+const body = document.querySelector('.bg');
+const red = document.querySelector('.red');
+const white = document.querySelector('.white');
+const yellow = document.querySelector('.yellow');
+const black = document.querySelector('.black');
+const green = document.querySelector('.green');
+const grey = document.querySelector('.grey');
+const orange = document.querySelector('.orange');
+const purple = document.querySelector('.purple');
+const pink = document.querySelector('.pink');
+const brown = document.querySelector('.brown');
+const khaki = document.querySelector('.khaki');
+const salmon = document.querySelector('.salmon');
+const gold = document.querySelector('.gold');
+const darkOrchid = document.querySelector('.dark-orchid');
+const silver = document.querySelector('.silver');
+
+blue.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'blue';
+});
+red.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'red';
+});
+white.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'white';
+});
+yellow.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'yellow';
+});
+black.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'black';
+});
+green.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'green';
+});
+grey.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'grey';
+});
+orange.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'orange';
+});
+purple.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'purple';
+});
+pink.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'pink';
+});
+brown.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'brown';
+});
+khaki.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'khaki';
+});
+salmon.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'salmon';
+});
+gold.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'gold';
+});
+darkOrchid.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'darkorchid';
+});
+silver.addEventListener('click', (e) =>{
+    body.style.backgroundColor = 'silver';
+});
